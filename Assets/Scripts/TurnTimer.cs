f9042cd56c2c6c36fc256ccbb307c7228f652d10
joeydawg7 <joeydawg7@gmail.com>
2017-04-30 23:39:55 -0400

    Functional move to click
    -- move to click works
    -- does a fun particle effect, too
    -- particle effect is kinda bad right now, wil be fixed  soon
    -- camera change to an orthographic veiw
    -- playrr looks where they move, causing bug where they look at the
    ground on reaching desintation
    
    NEXT STEPS: TIME!!!

--- Assets/Scripts/TurnTimer.cs
+++ Assets/Scripts/TurnTimer.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TurnTimer : MonoBehaviour {
+public class TurnTimer : MonoBehaviour
+{
 
     public GameObject sliderObj, player01, mousePointObj;
 
@@ -14,24 +15,34 @@ public class TurnTimer : MonoBehaviour {
 
     public float moveSpeed = 5;
 
-    private Stack<GameObject> pointObjQueue = new Stack<GameObject>();
+    private Queue<GameObject> pointObjQueue = new Queue<GameObject>();
+    private Queue<GameObject> pointEffectQueue = new Queue<GameObject>();
 
-	// Use this for initialization
-	void Start () {
+    public GameObject clickEffect;
+
+    private GameObject waypoint;
+    private GameObject effect;
+
+    // Use this for initialization
+    void Start()
+    {
 
         decreaseSlider = true;
 
         slider = sliderObj.GetComponent<Slider>();
 
+        waypoint = new GameObject();
+        effect = new GameObject();
 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        waypoint.transform.position = player01.transform.position;   
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
 
-        Camera.main.transform.LookAt(player01.transform);
+        //camera constantly looks at player
+        //Camera.main.transform.LookAt(player01.transform);
 
         if (slider.value == 0)
         {
@@ -49,71 +60,61 @@ public class TurnTimer : MonoBehaviour {
             slider.value += Time.deltaTime;
         }
 
+        //determine if the slider should go up
         if (slider.value == 0)
         {
             decreaseSlider = false;
-            StartCoroutine(startTurn());         
+            StartCoroutine(startTurn());
         }
 
-        if(slider.value ==5)
+        //determine if the slider should go down
+        if (slider.value == 5)
         {
             decreaseSlider = true;
             StartCoroutine(startTurn());
         }
 
-        /*
+
+
         if (Input.GetMouseButton(0))
         {
-            
-            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.z, player01.transform.position.y);
-            //Debug.Log(Input.mousePosition);
-            Vector3 lookPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            
-            lookPos = lookPos - transform.position;
-            mousePos.y = player01.transform.position.y;
-            mousePos.z =  Vector3.Distance(Input.mousePosition, Camera.main.transform.position);
-
-            float angle = Mathf.Atan2(lookPos.z, lookPos.x) * Mathf.Rad2Deg;
-
-            player01.transform.rotation = Quaternion.AngleAxis(angle, Vector3.down); // Turns Right
-            player01.transform.rotation = Quaternion.AngleAxis(angle, Vector3.up); //Turns Left
-            player01.transform.LookAt(mousePos);
-            player01.transform.position = Vector3.MoveTowards(player01.transform.position, mousePos, moveSpeed * Time.deltaTime);
-            Debug.DrawRay(player01.transform.position, mousePos,  Color.red, 1.0f);
-
-
-
-        }*/
-
-
-        if(Input.GetMouseButton(0))
-        {
-            // Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.z, player01.transform.position.y);
-            //Vector3 lookPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+            //mousePos
             Vector3 screenPoint = Input.mousePosition;
             screenPoint.z = 10.0f; //distance of the plane from the camera
+            //getting mouse point based on camera drama
             screenPoint = Camera.main.ScreenToWorldPoint(screenPoint);
-            //screenPoint.y = 1;
 
-            // mousePos.y = player01.transform.position.y;
-            // mousePos.z = Vector3.Distance(Input.mousePosition, Camera.main.transform.position);
-
-            //Instantiate(mousePointObj, mousePos, Quaternion.identity);
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray))
-               pointObjQueue.Push(Instantiate(mousePointObj, screenPoint, transform.rotation));
-
-
             
             
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+               // Debug.DrawLine(ray.direction, ray.origin, Color.green, 15.0f);
+
+                waypoint =  Instantiate(mousePointObj, hit.point, transform.rotation);
+
+                effect = Instantiate(clickEffect, hit.point, transform.rotation);
+
+                effect.GetComponent<ParticleSystem>().Emit(30);
+
+            }
+
 
         }
 
+        if (Vector3.Distance(player01.transform.position, waypoint.transform.position) > 1)
+        {
+            player01.transform.LookAt(waypoint.transform.position);
 
-        player01.transform.position = Vector3.MoveTowards(player01.transform.position, pointObjQueue.Peek().transform.position, moveSpeed * Time.deltaTime);
+            player01.transform.position = Vector3.MoveTowards(player01.transform.position, waypoint.transform.position, moveSpeed * Time.deltaTime);
+            effect.GetComponent<ParticleSystem>().Stop();
+        }
+
+
 
 
     }
@@ -121,10 +122,10 @@ public class TurnTimer : MonoBehaviour {
 
     IEnumerator startTurn()
     {
-        
+
         yield return new WaitForSeconds(2.0f);
-       
-        
+
+
     }
 }
 
